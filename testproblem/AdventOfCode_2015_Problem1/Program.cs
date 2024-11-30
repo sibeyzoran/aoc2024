35a5c060b1bffbfa80bd9aca8dcbc12a4611ebd5
James Martin <James.Martin@myriadtech.com.au>
2024-11-30 20:16:52 +1000

    Completed Day 1 2015 challenge as test

---
+++ testproblem/AdventOfCode_2015_Problem1/Program.cs
@@ -0,0 +1,25 @@
+﻿char up = '(';
+char down = ')';
+
+int floor = 0;
+int position = 1;
+
+string directions = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+
+foreach (char c in directions)
+{
+    if (c == up)
+    {
+        floor++;
+    }
+    else if (c == down)
+    {
+        floor--;
+    }
+    if (floor == -1)
+    {
+        Console.WriteLine($"Floor: {floor} Position: {position}");
+        return;
+    }
+    position++;
+}
