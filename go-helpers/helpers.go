44f4c086ab7f3340e6b43bcbe585050d84a72bbf
James Martin <James.Martin@myriadtech.com.au>
2024-12-07 17:07:16 +1000

    added to int to helpers

--- go-helpers/helpers.go
+++ go-helpers/helpers.go
@@ -1,6 +1,9 @@
 package helpers
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // ErrCheck checks for errors and prints them
 func ErrCheck(err error) {
@@ -8,4 +11,12 @@ func ErrCheck(err error) {
 		fmt.Println("Error:", err)
 		return
 	}
+}
+
+func ToInt(s string) int {
+	var number, err = strconv.Atoi(s)
+	if err != nil {
+		fmt.Println("Failed to convert string number to int")
+	}
+	return number
 }
